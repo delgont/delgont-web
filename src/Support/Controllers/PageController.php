b537df4090f8a1258b587a8ef6d1e71a91e11512
Stephen Delgont <stephen.delgont@outlook.com>
2022-07-01 13:51:29 +0300

    Update: Ability to show page with its associated posts

--- src/Support/Controllers/PageController.php
+++ src/Support/Controllers/PageController.php
@@ -2,8 +2,12 @@
 namespace Web\Support\Controllers;
 
 use Web\Support\Controllers\Concerns\ShowsPage;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
 
 abstract class PageController
 {
-    use ShowsPage;
+    use ValidatesRequests, AuthorizesRequests, ShowsPage;
+
 }
